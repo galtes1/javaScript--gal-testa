2e6f6261eaf983d6d7dfba2f9ba58b068ee13c1e
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-02-28 20:58:48 +0200

    24-02-28 REACT card intro

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import CardComponent from "./cards/components/CardComponent";
+const card = {
+  _id: "63765801e20ed868a69a62c4",
+  title: "first",
+  subtitle: "subtitle",
+  description: "testing 123",
+  phone: "050-0000000",
+  email: "test@gmail.com",
+  web: "https://www.test.co.il",
+  image: {
+    url: "assets/businessCard.jpg",
+    alt: "Business card image",
+  },
+  address: {
+    state: "",
+    country: "country",
+    city: "tel-aviv",
+    street: "Shinkin",
+    houseNumber: 3,
+    zip: 1234,
+  },
+  bizNumber: 1000000,
+  user_id: "63765801e20ed868a69a62c2",
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <CardComponent card={card} />;
 }
 
 export default App;
