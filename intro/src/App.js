fbbd44461cce7f78b806cb6301c4d8080a620843
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-03-01 14:39:22 +0200

    24-02-29 react home-task start

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,29 +1,31 @@
 import CardComponent from "./cards/components/CardComponent";
+import GalTasksPage from "./tasks/components/GalTasksPage";
 const card = {
-  _id: "63765801e20ed868a69a62c4",
-  title: "first",
-  subtitle: "subtitle",
-  description: "testing 123",
-  phone: "050-0000000",
-  email: "test@gmail.com",
-  web: "https://www.test.co.il",
-  image: {
-    url: "assets/businessCard.jpg",
-    alt: "Business card image",
-  },
-  address: {
-    state: "",
-    country: "country",
-    city: "tel-aviv",
-    street: "Shinkin",
-    houseNumber: 3,
-    zip: 1234,
-  },
-  bizNumber: 1000000,
-  user_id: "63765801e20ed868a69a62c2",
+ _id: "63765801e20ed868a69a62c4",
+ title: "first",
+ subtitle: "subtitle",
+ description: "testing 123",
+ phone: "050-0000000",
+ email: "test@gmail.com",
+ web: "https://www.test.co.il",
+ image: {
+  url: "assets/businessCard.jpg",
+  alt: "Business card image",
+ },
+ address: {
+  state: "",
+  country: "country",
+  city: "tel-aviv",
+  street: "Shinkin",
+  houseNumber: 3,
+  zip: 1234,
+ },
+ bizNumber: 1000000,
+ user_id: "63765801e20ed868a69a62c2",
 };
+
 function App() {
-  return <CardComponent card={card} />;
+ return <CardComponent card={card} />;
 }
 
 export default App;
---
+++ intro/src/tasks/components/GalTasksPage.jsx
@@ -0,0 +1,22 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import React from "react";
+
+const taskObject1 = {
+ name: "dishes",
+ date: "2024-01-30",
+ type: "chore",
+};
+const taskObject2 = {
+ name: "garbage",
+ date: "2024-01-28",
+ type: "chore",
+};
+export default function GalTasksPage() {
+ return <Table></Table>;
+}
