defe488334dd06e2ba2374129b38581d40b60e4c
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-03-31 21:26:59 +0300

    fix joi install

--- cardsProject/cards-d100923er/src/sandbox/CustomFormExample.jsx
+++ cardsProject/cards-d100923er/src/sandbox/CustomFormExample.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import { Box, Button, Paper, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+//const schema = Joi;
 
 export default function CustomFormExample() {
-  return <div>CustomFormExample</div>;
+  const [formDate, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.vale,
+    }));
+  };
+  return (
+    <div>
+      <Box component={Paper}>
+        <TextField
+          label="first name"
+          value={FormData.firstName}
+          name="firstName"
+          onChange={handleChange}
+        />
+        <TextField label="last name" />
+        <Button>submit</Button>
+      </Box>
+    </div>
+  );
 }
