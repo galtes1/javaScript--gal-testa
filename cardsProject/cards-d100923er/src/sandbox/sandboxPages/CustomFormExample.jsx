f2e7f71af333283125039ed0fe173a7608332237
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-04-03 17:42:39 +0300

    UPDATE

--- cardsProject/cards-d100923er/src/sandbox/sandboxPages/CustomFormExample.jsx
+++ cardsProject/cards-d100923er/src/sandbox/sandboxPages/CustomFormExample.jsx
@@ -3,43 +3,43 @@ import React, { useState } from "react";
 import Joi from "joi";
 
 const schema = Joi.object({
-  firstName: Joi.string().min(2),
-  lastName: Joi.string().min(2).max(10),
+ firstName: Joi.string().min(2),
+ lastName: Joi.string().min(2).max(10),
 });
 
 export default function CustomFormExample() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-  });
+ const [formData, setFormData] = useState({
+  firstName: "",
+  lastName: "",
+ });
 
-  const handleChange = (event) => {
-    setFormData((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
-  };
-  const handleSubmit = () => {
-    const validateObj = schema.validate(formData);
-    console.log(validateObj);
-  };
-  return (
-    <div>
-      <Box component={Paper}>
-        <TextField
-          label="first name"
-          value={formData.firstName}
-          name="firstName"
-          onChange={handleChange}
-        />
-        <TextField
-          label="last name"
-          value={formData.lastName}
-          name="lastName"
-          onChange={handleChange}
-        />
-        <Button onClick={handleSubmit}>submit</Button>
-      </Box>
-    </div>
-  );
+ const handleChange = (event) => {
+  setFormData((prev) => ({
+   ...prev,
+   [event.target.name]: event.target.value,
+  }));
+ };
+ const handleSubmit = () => {
+  const validateObj = schema.validate(formData);
+  console.log(validateObj);
+ };
+ return (
+  <Box>
+   <Box component={Paper}>
+    <TextField
+     label="first name"
+     value={formData.firstName}
+     name="firstName"
+     onChange={handleChange}
+    />
+    <TextField
+     label="last name"
+     value={formData.lastName}
+     name="lastName"
+     onChange={handleChange}
+    />
+    <Button onClick={handleSubmit}>submit</Button>
+   </Box>
+  </Box>
+ );
 }
